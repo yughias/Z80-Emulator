b7c655d06f7b492a4d133da508a6cf93c86266b2
yughias <sasa.laporta@gmail.com>
2025-02-13 21:44:03 +0100

    fixed z80 core, it now pass json tests

--- include/memory.h
+++ include/memory.h
@@ -5,24 +5,20 @@
 
 #include "z80.h"
 
-#define IO_SIZE 256
-
+#define IO_SIZE 0x10000
 #define MEMORY_SIZE 0x10000
 
 // I/0 SPACE
 extern uint8_t IO[IO_SIZE];
 
 //RAM
-extern uint8_t* MEMORY;
+extern uint8_t MEMORY[MEMORY_SIZE];
 
-void initMemory();
 void loadROM(const char*);
-void freeMemory();
 
 uint8_t readMemory(z80_t*, uint16_t);
 void writeMemory(z80_t*, uint16_t, uint8_t);
 uint8_t readIO(z80_t*, uint16_t);
 void writeIO(z80_t*, uint16_t, uint8_t);
-
 
 #endif
--- makefile
+++ makefile
@@ -1,7 +1,2 @@
 all:
-	gcc -Iinclude src/*.c -Ofast -o a.exe
-
-profile:
-	gcc -Iinclude -pg src/*.c -no-pie -o a.exe
-	a.exe
-	gprof -b a.exe gmon.out > analysis.txt
+	g++ -Iinclude -O2 src/*.cpp src/*.c
